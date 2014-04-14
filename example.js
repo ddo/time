81a836cd327edc568406d91565f4edbe575a4cce
Ddo <joeddo89@gmail.com>
2014-04-14 17:04:18 +0700

    adapt example

--- example.js
+++ example.js
@@ -1,3 +1,11 @@
-checkServerTime(5, function(err, good) {
+var time = Time(1000);
+
+time.getOffSet(function(err, offset) {
+    console.log('getOffSet()');
+    console.log(err, offset);
+});
+
+time.check(function(err, good) {
+    console.log('check()');
     console.log(err, good);
 });
